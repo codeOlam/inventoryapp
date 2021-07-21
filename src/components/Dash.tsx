b9d9de63a0df68fd072cbc0df546ca37e3833d41
codeolam <frankuchenna30@gmail.com>
2021-07-21 10:48:34 +0100

    added component files

---
+++ src/components/Dash.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Dash.css';
+
+function Dash() {
+  return (
+    <div className="Dash">
+      <h1>Home Page Coming Through</h1>
+    </div>
+  );
+}
+
+export default Dash;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Dash from './components/Dash';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Dash />
   </React.StrictMode>,
   document.getElementById('root')
 );
