16902e83c7676200b4707777816966e139405a95
codeolam <frankuchenna30@gmail.com>
2021-07-21 21:15:14 +0100

    updated file

--- src/components/Dash.tsx
+++ src/components/Dash.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import './Dash.css';
+import Products from './Products';
+
 
 function Dash() {
   return (
     <div className="Dash">
       <h1>Home Page Coming Through</h1>
+        <Products />
     </div>
   );
 }
