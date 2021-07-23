165fce3af88abe2ffe9cdd684c330d5981f2d197
codeolam <frankuchenna30@gmail.com>
2021-07-23 18:18:36 +0100

    added table

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react";
+import { Table } from "antd";
 import { listProducts } from "../graphql/queries";
 import { ListProductsQuery} from "../API";
 import actionGraphQL from "../action/GraphQlWrapper";
@@ -24,22 +25,42 @@ function Products(){
         getProducts()
     }, [])
 
+    const columns = [
+        {title: 'Name', dataIndex: 'name', key: 'name'},
+        {title: 'Price', dataIndex: 'price', key: 'price'},
+        {title: 'is Available?', dataIndex: 'inStock', key: 'inStock'},
+        {title: 'CatID', dataIndex: 'catID', key: 'catID'},
+        {title: 'Actions', dataIndex: '', key: 'x', render: () =><a>Delete</a>},
+    ];
+
+    const data = products?.map(row => ({
+        name: row.name,
+        price: row.price,
+        inStock: row.inStock,
+        catId: row.categoryID
+    }))
+
     return(
         <div>
             <h1>All Products</h1>
             {loading && <h2>Loading...</h2>}
             {
-                products?.map(product => (
+                 products?.map(product => ( 
+                    console.log("product: ", product),
                     <div key={product.id}>
                         <h2>Name: {product.name}</h2>
                         <h3>Price: {product.price} ||
-                            Available: {product.inStock.valueOf} ||
+                            Available: {product.inStock} ||
                             catID: {product.categoryID}
                         </h3>
                     </div>
                 ))
             }
-            <h3>Add New Product</h3>
+            <h3>Product Table</h3>
+            <Table 
+                columns={columns}
+                dataSource={data}
+            />
             <CreateProduct />
         </div>
 
